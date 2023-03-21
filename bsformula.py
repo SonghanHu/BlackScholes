83b23efcc78fd2dd278e60eef9c02b6f5b35860b
songhan hu <128550319+SonghanHu@users.noreply.github.com>
2023-03-21 15:32:33 -0700

    Update bsformula.py

--- bsformula.py
+++ bsformula.py
@@ -112,30 +112,6 @@ class date_to_day:
                     days += month[i]
             return days
 
-class logmode:
-
-    def __init__(self, s, mu, sigma, delta_t) -> None:
-
-        self.S = s
-        self.mu = mu
-        self.sigma = sigma
-        self.delta_t = delta_t
-
-    def logmode(self):
-
-        self.mode = self.S * np.exp((self.mu - (3/2)*self.sigma**2)*self.delta_t)
-        self.max = (1/(np.sqrt(2*np.pi)*self.S*self.sigma*np.sqrt(self.delta_t)))*np.exp((-self.mu+self.sigma**2)*(self.delta_t))
-        print(f'The stock price is most likely to be {self.mode} with probability{self.max}')
-
-    def proba(self, s):
-
-        self.mu1 = np.log(self.S) + (self.mu - (1/2) *self.sigma**2)*self.delta_t
-        self.sigma1 = self.sigma * np.sqrt(self.delta_t)
-        x = (np.log(s) - self.mu1)/self.sigma1
-        self.proba_less = stats.norm.cdf(x)
-        self.proba_more = 1 - self.proba_less
-        # print(f"Call_Out/Put_In:P = {self.proba_less},Call_In/ Put_Out: P = {self.proba_more}")
-
 class historical_val:
 
     def __init__(self, price_list) -> None:
@@ -153,22 +129,4 @@ class historical_val:
         self.sigma = np.sqrt((1 / (len(self.return_list) - 1)) * np.sum(((self.return_array - self.mu) ** 2)))
         self.mu_year = self.mu * 252
         self.sigma_year = self.sigma * np.sqrt(252)
-
-class bet:
-
-    def __init__(self):
-
-        pass
-
-    def calc_p(self,Oa,Ob):
-        self.x_1 = (1 + Ob ) / (2 + Oa + Ob)
-        self.p = (Oa * Ob-1)/(2 + Oa + Ob)
-        self.win_a = Oa/(1+Oa)
-        self.win_b = Ob/(1+Ob)
-        return (self.x_1, self.p)
-
-    def calc_O(self, win_a,win_b):
-
-        self.Oa = win_a/(1-win_a)
-        self.Ob = win_b/(1-win_b)
 
